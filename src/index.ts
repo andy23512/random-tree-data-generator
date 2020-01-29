3d653b08846510b3482bba7033fbc606c55c292a
andy23512 <andy23512@gmail.com>
2020-01-29 15:19:02 +0800

    Add implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('nanoha')
+export interface Config {
+  maxDepth: number;
+  minChildren: number;
+  maxChildren: number;
+  rootNumber: number;
+}
+
+const defaultConfig: Config = {
+  maxDepth: 3,
+  minChildren: 1,
+  maxChildren: 3,
+  rootNumber: 3
+};
+
+export interface TreeNode {
+  id: number;
+  children?: TreeNode[];
+}
+
+export function generateRandomTreeData(
+  config: Partial<Config> = {}
+): TreeNode[] {
+  const { maxDepth, minChildren, maxChildren, rootNumber } = {
+    ...config,
+    ...defaultConfig
+  };
+  let id = -1;
+  function genTree(level: number): TreeNode {
+    id += 1;
+    if (level === maxDepth) {
+      return {
+        id
+      };
+    }
+    const childrenNumber = Math.floor(
+      Math.random() * (maxChildren - minChildren + 1) + minChildren
+    );
+    return {
+      id,
+      children: new Array(childrenNumber)
+        .fill(null)
+        .map(() => genTree(level + 1))
+    };
+  }
+  return new Array(rootNumber).fill(null).map(() => genTree(1));
+}
---
+++ src/try.ts
@@ -0,0 +1,2 @@
+import { generateRandomTreeData } from './index';
+console.log(JSON.stringify(generateRandomTreeData(), null, 2));
